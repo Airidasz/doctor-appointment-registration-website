651cf1cc7f1bd3f459576531118a8c63c0d43a6b
Airidasz <61290246+Airidasz@users.noreply.github.com>
2020-12-07 11:55:10 +0200

    email is now only 30 characters max

--- database/migrations/2020_11_02_160600_create_users_table.php
+++ database/migrations/2020_11_02_160600_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
+            $table->string('email',30)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['patient', 'doctor', 'admin']);
             $table->rememberToken();
-            $table->timestamps();      
+            $table->timestamps();
         });
     }
 
